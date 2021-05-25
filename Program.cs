e042668ec120ef8c4ea3c9559aab1239dfbabd27
Jeremiah Schugt <jeremiah.r.schugt@gmail.com>
2021-05-25 10:32:56 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+void main()
+{
+    Console.Write("Guess my secret number: ");
+    int userGuess = 0;
+    while (!int.TryParse(Console.ReadLine(), out userGuess))
+    {
+        Console.Write("I need an integer! ");
+    }
+    Console.WriteLine($"You guessed {userGuess}");
+}
+
+main();
