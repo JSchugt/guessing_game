53537691a585fc13efb21d55123da9fe944a55be
Jeremiah Schugt <jeremiah.r.schugt@gmail.com>
2021-05-25 11:26:27 -0500

    add difficulty levels

--- Program.cs
+++ Program.cs
@@ -7,17 +7,39 @@ int numberValidator(int userGuess)
     }
     return userGuess;
 }
+int difficultyValidator(int userGuess)
+{
+    while (!int.TryParse(Console.ReadLine(), out userGuess) && userGuess < 1 && userGuess > 3)
+    {
+        Console.Write("I need an integer! between 1 and 3: ");
+    }
+    if (userGuess == 1)
+    {
+        userGuess = 8;
+    }
+    else if (userGuess == 2)
+    {
+        userGuess = 6;
+    }
+    else
+    {
+        userGuess = 4;
+    }
+    return userGuess;
+}
 void main()
 {
+    Console.WriteLine("What difficulty would you like \n 1. Easy \n 2. Medium \n 3. Hard");
+    int difficulty = difficultyValidator(0);
     int counter = 1;
-    Console.Write($"You have 4 guesses \n Guess my secret number ({counter}): ");
+    Console.Write($"You have {difficulty} guesses \n Guess my secret number ({counter}): ");
     Random gen = new Random();
 
     int userGuess = 0;
     int theNumber = gen.Next(1, 100);
     userGuess = numberValidator(userGuess);
     bool correctGuess = false;
-    while (counter < 4 && !correctGuess)
+    while (counter < difficulty && !correctGuess)
     {
         counter++;
         if (theNumber == userGuess)
@@ -29,7 +51,7 @@ void main()
         {
             string range = (userGuess > theNumber) ? "Your guess was too high" : "Your guess was too low";
             Console.WriteLine(range);
-            Console.WriteLine($"You have {5 - counter} guesses left!");
+            Console.WriteLine($"You have {difficulty - counter + 1 } guesses left!");
             Console.Write($"Guess again({counter}): ");
             userGuess = numberValidator(userGuess);
         }
